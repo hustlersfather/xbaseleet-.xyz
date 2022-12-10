15ba0d5d03673904990ca6e260c9a323ea10517b
xmarveloader <75556276+marathonloader@users.noreply.github.com>
2022-12-10 17:53:45 +0100

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php
 
 
-$dbcon = mysql_connect("localhost","jeruueve_nexus","blid12345") or die("mysql not connected");
-$dbselect = mysql_select_db("jeruueve_nexus",$dbcon) or die("database not found");
+$dbcon = mysql_connect("localhost","hustlersfather_jeruueve_nexus","hustlersfather_jeruueve_nexus_blid12345") or die("mysql not connected");
+$dbselect = mysql_select_db("hustlersfather_jeruueve_nexus",$dbcon) or die("database not found");
 
 
 
