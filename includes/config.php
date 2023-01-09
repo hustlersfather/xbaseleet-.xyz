4bee0f0fda4ee965b1170fadf8ba81c00db25fa6
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-01-09 19:31:46 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$dbcon = mysqli_connect("localhost","hustlersfather_artmir","hustlersfather_Omeri1233","hustlersfather_artmir") or die('enable connection'); 
+$dbcon = mysqli_connect("localhost","hustlersfather_artmir","hustlersfather_artmir","hustlersfather_artmir") or die('enable connection'); 
 
 
 
